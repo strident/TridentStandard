d81fec6e4c3e150cf7de16efc6723e4199f085bd
Elliot Wright <wright.elliot@gmail.com>
2014-05-24 23:24:42 +0100

    Basic framework.

---
+++ app/TridentKernel.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of Trident.
+ *
+ * (c) Elliot Wright <elliot@elliotwright.co>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Trident\Component\HttpKernel\HttpKernel;
+
+/**
+ * Application Kernel
+ *
+ * Loads all of the application's modules, allows things like module service
+ * configuration to happen.
+ */
+class TridentKernel extends HttpKernel
+{
+    /**
+     * Register modules
+     *
+     * @param  string $environment
+     * @return array
+     */
+    public function registerModules($environment)
+    {
+        $modules = [
+            new Trident\Module\FrameworkModule\TridentFrameworkModule()
+        ];
+
+        if (in_array($environment, array('dev'))) {
+            // Register development modules here:
+        }
+
+        return $modules;
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,64 +1,28 @@
 <?php
 
-$loader = require_once __DIR__.'/../vendor/autoload.php';
+/**
+ * Use APC loader if the module is available, remember to change 'Strident' to
+ * something unique if you have more than one copy of Strident installed on
+ * your server, or if you're using shared hosting.
+ */
+// use Symfony\Component\ClassLoader\ApcClassLoader;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
 
-use Phimple\Container;
-use Phimple\LockBox;
+/**
+ * @var ClassLoader $loader
+ */
+$loader = require_once __DIR__.'/../app/autoload.php';
+// $loader = new ApcClassLoader('Trident', $loader);
 
-$container = new Container();
-$container->set('test', function($c) {
-    $lockbox = new LockBox();
-    $lockbox->set('name', 'test'); // Just to identify this lockbox in DIC
-    return $lockbox;
-});
+// Debug::enable();
 
-$container->set('factory', $container->factory(function($c) {
-    // This is where the real test begins...
-    $lockbox = new LockBox();
-    $lockbox->set('name', 'factory'); // Just to identify this lockbox in DIC
-    return $lockbox;
-}));
+require_once __DIR__.'/../app/TridentKernel.php';
+$kernel = new TridentKernel(true);
 
-// Make test default to having an ID of 1
-$container->extend('test', function($test, $c) {
-    $test->set('id', 1);
+$request = Request::createFromGlobals();
 
-    return $test;
-});
+$response = $kernel->handle($request);
+$response->send();
 
-// Make factory default to having an ID of 1
-$container->extend('factory', function($factory, $c) {
-    $factory->set('id', 1);
-
-    return $factory;
-});
-
-var_dump("Container State - Pre-fetch");
-var_dump($container);
-var_dump("Service returned from service");
-
-// This should make the changes on both objects
-$service1 = $container->get('test');
-$service2 = $container->get('test');
-
-// This should make both service1 and service2 have an ID of 2
-$service2->set('id', 2);
-
-var_dump($service1);
-var_dump($service2);
-
-var_dump("Factory returned from service");
-
-// This should let me make 2 completely different objects
-$factory1 = $container->get('factory');
-$factory2 = $container->get('factory');
-
-// This should only make factory2 have an ID of 2
-$factory2->set('id', 2);
-
-var_dump($factory1);
-var_dump($factory2);
-
-var_dump("Container State - Post-fetch");
-var_dump($container);
-exit;
+$kernel->terminate($request, $response);
