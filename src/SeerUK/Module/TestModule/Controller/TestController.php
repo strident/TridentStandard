abfda429ae5a3e28a5a2b7ca8d5aff1688822107
Elliot Wright <wright.elliot@gmail.com>
2014-05-28 00:43:34 +0100

    Testing template name resolution.

--- src/SeerUK/Module/TestModule/Controller/TestController.php
+++ src/SeerUK/Module/TestModule/Controller/TestController.php
@@ -5,10 +5,17 @@ namespace SeerUK\Module\TestModule\Controller;
 use Phalcon\Http\Response;
 use Trident\Module\FrameworkModule\Controller\Controller;
 
+use Trident\Component\Templating\TemplateNameResolver;
+
 class TestController extends Controller
 {
     public function testAction()
     {
-        return new Response('Hello world!', 200);
+        $resolver  = new TemplateNameResolver($this->container->get('kernel'));
+        $reference = $resolver->resolve('::index.html.twig');
+
+        var_dump($reference->getPath());
+
+        return new Response();
     }
 }
