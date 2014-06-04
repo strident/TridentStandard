6cf942e4268de3463babcc33f39ae36edbee575d
Elliot Wright <wright.elliot@gmail.com>
2014-06-05 00:05:41 +0100

    Best to keep this in sync with the Trident repo at least...

--- src/SeerUK/Module/TestModule/Controller/TestController.php
+++ src/SeerUK/Module/TestModule/Controller/TestController.php
@@ -15,6 +15,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Trident\Module\FrameworkModule\Controller\Controller;
 
 use SeerUK\Module\TestModule\Data\Entity\User;
+use Trident\Component\Caching\CachingProxy;
 
 /**
  * Test Controller
@@ -25,17 +26,51 @@ class TestController extends Controller
 {
     public function testAction()
     {
-        // $caching = $this->get('caching');
-        // var_dump($caching);
-        // exit;
-
         $start = microtime(true);
 
-        // $em   = $this->get('doctrine.orm.entity_manager');
+        $caching = $this->get('caching');
+        $repo    = $this->get('test.repository.user');
+        $proxy   = new CachingProxy();
+        $proxy->setDriver($caching);
 
-        $repo = $this->get('test.repository.user');
-        $users = $repo->findAll();
-        $last  = end($users);
+        var_dump($proxy);
+
+        $users = $proxy->get('users', function() use($repo) {
+            return $repo->findAll();
+        });
+
+        var_dump($users);
+        exit;
+
+        // Create ProxyCache.php
+        //
+        // This class will accept a name, and a closer. The name will be the
+        // cache key, the closure will contain the 'real' data source.
+        //
+        // From this proxy cache, a single method may be run to get the value.
+        // The proxy cache will be set up with the cache factory.
+
+        $real = function($container) {
+            return $container->get('test.repository.user')->findAll();
+        };
+
+        $getUsers = function($container, $key, \Closure $real) {
+            $caching = $container->get('caching');
+
+            if ($caching->has($key)) {
+                return $caching->get($key);
+            }
+
+            $data = $real($container);
+            $caching->set($key, $data);
+
+            return $data;
+        };
+
+        var_dump($getUsers);
+        var_dump(count($getUsers($this->container, 'users', $real)));
+
+        // $em   = $this->get('doctrine.orm.entity_manager');
 
         // $user = new User();
         // $user->setUsername('Test' . ($last->getId() + 1));
@@ -47,7 +82,7 @@ class TestController extends Controller
         $end = microtime(true);
 
         // var_dump($users);
-        var_dump(count($users));
+        // var_dump(count($users));
         var_dump(($end - $start) * 1000);
 
         return $this->render('SeerUKTestModule:Test:index.html.twig', [
