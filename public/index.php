13f3cbd15fc320624046407e3631a1fb434393e4
Elliot Wright <wright.elliot@gmail.com>
2014-06-07 14:33:24 +0100

    Made front controller smaller.

--- public/index.php
+++ public/index.php
@@ -1,20 +1,11 @@
 <?php
 
-use Symfony\Component\Debug\Debug;
-use Symfony\Component\HttpFoundation\Request;
-
-/**
- * @var ClassLoader $loader
- */
 $loader = require_once __DIR__.'/../app/autoload.php';
-
 require_once __DIR__.'/../app/TridentKernel.php';
 
-// Debug::enable();
-
-$kernel = new TridentKernel('dev', TridentKernel::DEBUG_DISABLED);
-// $kernel = new TridentKernel('dev', TridentKernel::DEBUG_ENABLED);
-$request = Request::createFromGlobals();
+$request = Symfony\Component\HttpFoundation\Request::createFromGlobals();
+$kernel  = new TridentKernel('dev', TridentKernel::DEBUG_DISABLED);
+// $kernel  = new TridentKernel('dev', TridentKernel::DEBUG_ENABLED);
 
 $response = $kernel->handle($request);
 $response->send();
