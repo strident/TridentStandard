c74b4b130163dba7efb1ac208918c48de190672f
Elliot Wright <wright.elliot@gmail.com>
2014-06-21 14:59:51 +0100

    Keep in sync with Trident for testing on production server.

--- public/index.php
+++ public/index.php
@@ -5,8 +5,8 @@ require_once __DIR__.'/../app/TridentKernel.php';
 
 $request = Trident\Component\HttpFoundation\Request::createFromGlobals();
 $kernel  = new TridentKernel('prod', TridentKernel::DEBUG_ENABLED);
-$kernel  = new TridentKernel('dev', TridentKernel::DEBUG_DISABLED);
 $kernel  = new TridentKernel('dev', TridentKernel::DEBUG_ENABLED);
+$kernel  = new TridentKernel('dev', TridentKernel::DEBUG_DISABLED);
 $kernel  = new TridentKernel('prod', TridentKernel::DEBUG_DISABLED);
 
 $response = $kernel->handle($request);
