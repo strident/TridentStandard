711516ec7565a4ea7c9803b5c43b135bfdc84acd
Elliot Wright <wright.elliot@gmail.com>
2014-07-08 00:24:17 +0100

    Added new dev front-controller.

---
+++ public/index_dev.php
@@ -0,0 +1,13 @@
+<?php
+
+$loader = require_once __DIR__.'/../app/autoload.php';
+require_once __DIR__.'/../app/TridentKernel.php';
+
+$request = Trident\Component\HttpFoundation\Request::createFromGlobals();
+$kernel  = new TridentKernel('dev', TridentKernel::DEBUG_DISABLED);
+$kernel  = new TridentKernel('dev', TridentKernel::DEBUG_ENABLED);
+
+$response = $kernel->handle($request);
+$response->send();
+
+$kernel->terminate($request, $response);
